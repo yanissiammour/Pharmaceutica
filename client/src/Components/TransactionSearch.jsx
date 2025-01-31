c0c61e478c23d49293f571ae825d811a746ca647
Nagi.akc <167807729+samyAkchiche@users.noreply.github.com>
2025-01-31 22:56:16 +0100

    Branch1 (#12)
    * Finished working with the cardClient component
    
    * Make ClientTable components & waiting for the backend
    
    * Finished Working on the productspage(ProductCard & ProductTable)
    
    * Added a searchbar(not functional yet), and a table for TransactionsLog
    
    * Added a filter button(asending & desending) and a loader for the search button
    
    * Corriged typo

--- client/src/Components/TransactionSearch.jsx
+++ client/src/Components/TransactionSearch.jsx
@@ -1,7 +1,16 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faArrowUpShortWide,
+    faArrowDownWideShort,
+} from "@fortawesome/free-solid-svg-icons";
+import { Loader2 } from "lucide-react";
+import { useState } from "react";
 
 export default function TransactionSearch() {
+    const [isAsending, setisAsending] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
     return (
         <div className="flex w-full max-w-sm items-center space-x-2">
             <Input
@@ -9,7 +18,16 @@ export default function TransactionSearch() {
                 placeholder="What are you searching for ?"
                 className="bg-white"
             />
-            <Button type="submit">Search</Button>
+            <Button type="submit" onClick={() => setIsLoading(!isLoading)}>
+                {isLoading ? <Loader2 className="animate-spin" /> : "Search"}
+            </Button>
+            <Button onClick={() => setisAsending(!isAsending)}>
+                <FontAwesomeIcon
+                    icon={
+                        isAsending ? faArrowUpShortWide : faArrowDownWideShort
+                    }
+                />
+            </Button>
         </div>
     );
 }
